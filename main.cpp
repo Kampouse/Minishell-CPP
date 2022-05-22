d8555f8e8e2d7765a4bfb82f3892d60cc3a5393a
Kampouse <jpmartel98@gmail.com>
2022-05-22 04:15:31 +0200

    looping

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include	"iostream"
 #include	"cstdlib"
 #include	"readline/readline.h"
+
+std::string *ReadLine(const char *display)
+{
+	char *str	 = readline(display);
+	std::string Str(str);
+	free(str);
+return(new std::string(Str));
+}
+
 int main(void)
 {
-
-char *str;
-str  = readline("noah");
-free(str);
+		while(1)
+	{
+		std::string *value = ReadLine(">");
+		std::cout << *value << std::endl;
+		if(*value  ==  "exit")		
+		{
+			delete  value;
+			break;
+		}
+	
+		delete  value;
+	}
 }
