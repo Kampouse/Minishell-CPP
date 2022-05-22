b01f2ca29d58fccb42167922a73268b1072f8b88
Kampouse <jpmartel98@gmail.com>
2022-05-22 23:27:36 +0200

    made it so it can read token while skipping space does not handle any quote at the moment

--- main.cpp
+++ main.cpp
@@ -10,18 +10,38 @@ std::string *ReadLine(const char *display)
 return(new std::string(Str));
 }
 
+
+void skip_space(std::string::iterator&elem,std::string*str)
+{
+		while( elem != str->end() && std::isspace(*elem))
+				elem++;
+
+}
+
+
+void find_end(std::string::iterator&elem,std::string*str)
+{
+		while( elem != str->end() && !std::isspace(*elem))
+				elem++;
+}
+
 int main(void)
 {
 		while(1)
 	{
 		std::string *value = ReadLine(">");
-		std::cout << *value << std::endl;
+		std::string::iterator begin =  value->begin();
+		std::string::iterator end =  value->begin();
+		skip_space(end,value);
+		begin = end;
+		find_end(end,value);
+		std::cout <<"("	<< std::string(begin,end) << ")" << std::endl;
 		if(*value  ==  "exit")		
 		{
 			delete  value;
 			break;
-		}
-	
+		} 
+		
 		delete  value;
 	}
 }
